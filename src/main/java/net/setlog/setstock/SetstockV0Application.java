b840f6fa91a66ca3ff9271ff7ad3456f53e3f551
JUKI-J <jju.ki@hotmail.com>
2025-03-06 11:53:58 +0900

    Initial commit

---
+++ src/main/java/net/setlog/setstock/SetstockV0Application.java
@@ -0,0 +1,13 @@
+package net.setlog.setstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetstockV0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetstockV0Application.class, args);
+    }
+
+}
---
+++ src/test/java/net/setlog/setstock/SetstockV0ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.setlog.setstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetstockV0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
